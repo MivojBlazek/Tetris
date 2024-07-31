7548aa631f3433b002c4211c3534cdfbc5370d14
MivojBlazek <MivojBlazek@seznam.cz>
2024-07-31 18:02:19 +0200

    Updated block detection and top of the tower detection Blocks are falling one by one and can detect others. User can speed up game. If block reaches top of the tower, game will end.

--- src/block.cpp
+++ src/block.cpp
@@ -4,11 +4,13 @@
 #include <QPainter>
 #include <QDebug>
 #include <QPen>
+#include <QColor>
 
 Block::Block(QGraphicsItem *parent)
-    : QGraphicsRectItem{parent}
+    : QGraphicsRectItem{parent},
+      mColor{QColor(150, 150, 150, 128)}
 {
-     setRect(200, 0, 40, 40);
+     setRect(200 + MARGIN / 2, 0 + MARGIN / 2, 40 - MARGIN, 40 - MARGIN);
 }
 
 void Block::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
@@ -19,30 +21,41 @@ void Block::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWi
     painter->setPen(QPen(Qt::black));
     painter->drawRect(boundingRect());
 
-    QColor fillColor = QColor(150, 150, 150, 128);
+    QColor fillColor = mColor;
     painter->fillRect(boundingRect(), fillColor);
 }
 
-void Block::moveDown()
+int Block::moveDown(qreal step)
 {
     if (scenePos().y() < 760.0)
     {
-        setPos(pos() + QPointF(0, 40));
+        setPos(pos() + QPointF(0, step));
     }
+    else
+    {
+        return 1;
+    }
+    return 0;
 }
 
-void Block::moveLeft()
+void Block::moveLeft(qreal step)
 {
     if (scenePos().x() + 200 > 0.0 && scenePos().y() < 760.0)
     {
-        setPos(pos() + QPointF(-40, 0));
+        setPos(pos() + QPointF(-step, 0));
     }
 }
 
-void Block::moveRight()
+void Block::moveRight(qreal step)
 {
     if (scenePos().x() + 200 < 360.0 && scenePos().y() < 760.0)
     {
-        setPos(pos() + QPointF(40, 0));
+        setPos(pos() + QPointF(step, 0));
     }
+}
+
+void Block::changeColor(QColor color)
+{
+    mColor = color;
+    update();
 }
